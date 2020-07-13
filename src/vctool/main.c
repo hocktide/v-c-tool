6765902a0b096e26cb0d0157504cd932981f7bec
Justin Handville <nanolith@gmail.com>
2020-07-12 21:35:38 -0400

    Added Meson build scripts for vctool.

---
+++ src/vctool/main.c
@@ -0,0 +1,25 @@
+/**
+ * \file vctool/main.c
+ *
+ * \brief Main entry point for the vctool utility.
+ *
+ * \copyright 2020 Velo Payments.  See License.txt for license terms.
+ */
+
+#include <stdio.h>
+
+/**
+ * \brief Main entry point for vctool.
+ *
+ * \param argc      The number of arguments.
+ * \param argv      Arguments to main.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    return 0;
+}
